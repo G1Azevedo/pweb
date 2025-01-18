b2e277549b311409a66f3017b2bea68096c0c9c6
Gabriel Azevedo <gabriel.araujo2706@gmail.com>
2025-01-18 14:41:12 -0300

    receita 16

---
+++ unidade_II/next_04/src/app/novarota/components/header.jsx
@@ -0,0 +1,5 @@
+export function HeaderComponent() {
+    return (
+        <h1>SISTEMA | SISTEMA GRANDE | MENU </h1>
+    )
+}
---
+++ unidade_II/next_04/src/app/novarota/subrota/page.tsx
@@ -0,0 +1,7 @@
+export default function NovaSubRotaHome() {
+    return (
+        <div>
+            <h1>ACREDITO QUE SEJA A SUB-ROTA</h1>
+        </div>
+    )
+}
